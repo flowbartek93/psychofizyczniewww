d9ba05a2421e2fd328586b6ccb3bb40d97e023c0
Bartek <submission93@gmail.com>
2021-03-23 10:50:18 +0100

    hamburger menu

--- scripts/Hamburger.js
+++ scripts/Hamburger.js
@@ -1,3 +1,24 @@
 class Hamburger {
-  constructor() {}
+  constructor() {
+    this.lines = document.querySelectorAll(".line");
+    this.hamburgerBtn = document.querySelector(".nav__hamburger");
+    this.navmobile = document.querySelector(".nav-mobile");
+    this.events();
+  }
+
+  events() {
+    this.hamburgerBtn.addEventListener("click", () => {
+      this.toggle();
+    });
+  }
+  toggle() {
+    this.lines.forEach((line) => {
+      line.classList.toggle("active");
+    });
+
+    this.navmobile.classList.toggle("active");
+    console.log("ok");
+  }
 }
+
+const run = new Hamburger();
